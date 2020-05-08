169ee792a6b1d0952ac7ee2d43aea7f98f7bfd07
Jonathon Gore <jgore@uwaterloo.ca>
2020-05-08 15:12:14 -0500

    Adding controlled value to text input

--- src/SDFormTextItem.js
+++ src/SDFormTextItem.js
@@ -8,19 +8,21 @@ type Props = {|
   name: string,
   password?: boolean,
   placeholder?: string,
+  value?: string,
 |};
 
 function SDFormTextItem(props: Props): Node {
-  const { label, name, password, placeholder } = props;
+  const { label, name, password, placeholder, value } = props;
 
   return (
     <div className="sd-form-item">
-      <SDText size={"header2"}>{label}</SDText>
+      <SDText size="header2">{label}</SDText>
       <input
         className="sd-form-text-input"
         name={name}
         placeholder={placeholder ?? ""}
         type={password ? "password" : "text"}
+        value={value}
       />
     </div>
   );
--- src/SDLink.js
+++ src/SDLink.js
@@ -5,14 +5,19 @@ import React from "react";
 type Props = {|
   children: string,
   href: string,
+  hideUnderline?: boolean,
 |};
 
 // SDLink is to be used within an SDText element.
 function SDLink(props: Props) {
-  const { children, href } = props;
+  const { children, hideUnderline, href } = props;
 
+  const classNames = ["sd-link"];
+  if (!hideUnderline) {
+    classNames.push("sd-link-underline");
+  }
   return (
-    <a className="sd-link" href={href}>
+    <a className={classNames.join(" ")} href={href}>
       {children}
     </a>
   );
